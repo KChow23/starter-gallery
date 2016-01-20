33884e9f9812a5d0de7227db0c3df1407fe46b67
KChow23 <kchow_21@hotmail.com>
2016-01-20 11:21:02 -0800

    Changed the directory referece to /data

--- application/views/_cell.php
+++ application/views/_cell.php
@@ -7,6 +7,6 @@
  */
 ?>
 
-<a href="./data/{filename}" data-lightbox="gallery" data-title="{title} ... Posted {uploaded} by {uploaded}, in {category}">
-    <img src="./data/thumb/{filename}"/>
+<a href="/data/{filename}" data-lightbox="gallery" data-title="{title} ... Posted {uploaded} by {uploaded}, in {category}">
+    <img src="/data/thumb/{filename}"/>
 </a>
